294a018b4fe32619d1560d795c023cba5ab4328a
Javier Ntaca <japedev@gmail.com>
2024-04-12 15:03:10 -0300

    upddater log to file

--- updater.js
+++ updater.js
@@ -1,36 +1,50 @@
-console.log("UPDATER START")
+
 import fs from 'fs';
 import os, { hostname } from 'os'
 import { io } from 'socket.io-client';
 import { pid } from 'process';
 const client_socket = io("http://167.172.44.239:46302");
 import { execFile } from 'node:child_process';
-console.log("pid", pid)
+try {
+  fs.rmSync("updater_log.txt", { force: true })
+} catch (error) {
+
+}
+log_to_file("UPDATER START")
+log_to_file("pid", pid)
 fs.writeFileSync("pid.txt", pid.toString())
 client_socket.on("UPDATE_APP", (data) => {
   if (os.hostname() == 'DESKTOP-S4RPOTO') {
-    console.log("en casa noooo")
+    log_to_file("en casa noooo")
     return
   }
 
   const child_git = execFile('git ', ['pull'], (error, stdout, stderr) => {
-    console.log("stderr lanzar git", stderr);
+    log_to_file("stderr lanzar git", stderr);
     if (error) {
-      console.log("error lanzando git", error)
+      log_to_file("error lanzando git", error)
     }
-    console.log("output de git pull", stdout);
-    console.log("lanzo node index")
+    log_to_file("output de git pull", stdout);
+    log_to_file("lanzo node index")
     const child_start = execFile('node', ['./index.js'], (error, stdout, stderr) => {
-      console.log("stderr lanzar node index", stderr);
+      log_to_file("stderr lanzar node index", stderr);
       if (error) {
-        console.log("error lanzando node index", error)
+        log_to_file("error lanzando node index", error)
       }
-      console.log("output de lanzar node index", stdout)
+      log_to_file("output de lanzar node index", stdout)
     });
 
   });
 })
 client_socket.on('connect', () => {
-  console.log('updater socket connected');
+  log_to_file('updater socket connected');
   client_socket.emit('updater_register', os.hostname())
 });
+
+function log_to_file() {
+  let str = ""
+  for (let i = 0; i < arguments.length; i++) {
+    str += " " + arguments[i]
+  }
+  fs.appendFileSync("updater_log.txt", str + "\n")
+}
